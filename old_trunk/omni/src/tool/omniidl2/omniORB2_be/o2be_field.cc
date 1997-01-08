d01365ddf4c0e32612c62e65dd42ac2455a1e53f
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1997-01-08 17:48:07 +0000

    Initial revision

---
+++ old_trunk/omni/src/tool/omniidl2/omniORB2_be/o2be_field.cc
@@ -0,0 +1,25 @@
+//                          Package   : omniidl
+// o2be_field.cc            Created on: 9/8/96
+//			    Author    : Sai-Lai Lo (sll)
+//
+// Copyright (C) Olivetti Research Limited, 1996
+//
+// Description:
+
+/*
+  $Log$
+  */
+
+#include <idl.hh>
+#include <idl_extern.hh>
+#include <o2be.h>
+
+o2be_field::o2be_field(AST_Type *ft, UTL_ScopedName *n, UTL_StrList *p)
+	: AST_Field(ft, n, p),
+	  AST_Decl(AST_Decl::NT_field, n, p),
+	  o2be_name(this)
+{
+}
+
+IMPL_NARROW_METHODS1(o2be_field, AST_Field)
+IMPL_NARROW_FROM_DECL(o2be_field)
