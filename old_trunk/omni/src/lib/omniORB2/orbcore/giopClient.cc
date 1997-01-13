5c387729c602f7b5faea5071adba8d603b412f26
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1997-01-13 15:01:59 +0000

    If the reply message size in the header is too large, throw a COMM_FAILURE instead of omniORB::fatalException.

--- old_trunk/omni/src/lib/omniORB2/orbcore/giopClient.cc
+++ old_trunk/omni/src/lib/omniORB2/orbcore/giopClient.cc
@@ -11,6 +11,9 @@
  
 /*
   $Log$
+  Revision 1.1  1997/01/08 17:26:01  sll
+  Initial revision
+
   */
 
 #include <omniORB2/CORBA.h>
@@ -241,8 +244,10 @@ GIOP_C::RequestCompleted(CORBA::Boolean skip_msg)
   else
     {
       if (RdMessageUnRead())
-	throw omniORB::fatalException(__FILE__,__LINE__,
-             "GIOP_C::RequestCompleted() reported wrong reply message size.");				  
+	{
+	  setStrandDying();
+	  throw CORBA::COMM_FAILURE(0,CORBA::COMPLETED_MAYBE);
+	}
     }
 
   pd_state = GIOP_C::Idle;
