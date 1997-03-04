0632b9bfce504bbe5408fb510fd00d7b8e8098b1
ewc <ewc@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1997-03-04 11:08:25 +0000

    Added support for obtaining initial object reference from Windows NT registry

--- old_trunk/omni/src/lib/omniORB/orbcore/initFile.h
+++ old_trunk/omni/src/lib/omniORB/orbcore/initFile.h
@@ -11,18 +11,17 @@
 
 /*
   $Log$
+ * Revision 1.2  1997/01/21  15:03:00  ewc
+ * Minor change - moved #define from source file to header.
+ *
   */
 
 
 // Class to read and store constants from initialization file.
-// Used for ATMos and UNIX.
+// Used for ATMos, UNIX and Windows NT.
 
 #ifndef __INITFILE_H__
 #define __INITFILE_H__
-
-
-#define MAX_CONFIG 10   
-
 
 class initFile
 {
@@ -32,7 +31,6 @@ public:
   virtual ~initFile();
 
   void initialize();
-  
 
 private:
   char* fData;
@@ -42,10 +40,25 @@ private:
   inline void multerr(char* entryname);      
   inline void dataerr(char* entryname);
   inline void parseerr();
-  
+ 
+#ifdef __NT__ 
+// NT-specific error reporting functions:
+  inline void noValsFound();
+  inline void formaterr(char* entryname);
+#endif
 
+  int read_file(char* config_fname);
   int getnextentry(char*& entryname, char*& data);
  
+#ifdef __NT__
+  int use_registry;
+ 
+  HKEY init_hkey;
+  DWORD init_maxValLen;
+  DWORD init_maxDataLen;
+
+  int getRegistryEntry(char*& entryname, char*& data);
+#endif
 
 public:
   
