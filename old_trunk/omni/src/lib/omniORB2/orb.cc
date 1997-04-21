53f33e52db735ac1ca377e5ffa31f4bfbd4104d3
ewc <ewc@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1997-04-21 10:24:00 +0000

    Minor cosmetic change.

--- old_trunk/omni/src/lib/omniORB2/orb.cc
+++ old_trunk/omni/src/lib/omniORB2/orb.cc
@@ -11,6 +11,13 @@
  
 /*
   $Log$
+// Revision 1.6  1997/03/10  12:10:20  sll
+// - Support for ORB and BOA initialisation arguments.
+// -  Add runtime configurable trace messages.
+// - omni::init will only throws CORBA::INITIALIZE exception if anything goes
+//   wrong.
+// - Minor cleanup to accomodate the creation of a public API for omniORB2.
+//
 // Revision 1.5  1997/02/19  11:03:29  ewc
 // Added support for NT
 //
@@ -163,7 +170,6 @@ omni::init(int &argc,char **argv,const char *orb_identifier)
       }
 
     // Get configuration information:
-    // XXXXX: To be replaced by System Registry functions
 
     configFile = new initFile;
     configFile->initialize();
