199f887a4253f4eb7fc883bb1a088c574c25652a
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1997-12-23 19:29:37 +0000

    Now generate the implementation of array helper functions defined in the global scope in the skeleton file. This applies to typedef of array as well.

--- old_trunk/omni/src/tool/omniidl2/omniORB2_be/o2be_typedef.cc
+++ old_trunk/omni/src/tool/omniidl2/omniORB2_be/o2be_typedef.cc
@@ -27,6 +27,9 @@
 
 /*
   $Log$
+  Revision 1.5  1997/12/09 19:55:01  sll
+  *** empty log message ***
+
   Revision 1.4  1997/08/27 17:55:07  sll
   If the true type of an interface is Object, use CORBA::Object_member as
   the field member type name.
@@ -138,8 +141,18 @@ o2be_typedef::produce_skel(fstream &s)
 {
   AST_Decl *decl = base_type();
 
+  while (decl->node_type() == AST_Decl::NT_typedef) {
+    decl = o2be_typedef::narrow_from_decl(decl)->base_type();
+  }
+
   if (decl->node_type() == AST_Decl::NT_array)
-    o2be_array::narrow_from_decl(decl)->produce_skel(s,this);
+    {
+      if (base_type()->node_type() == AST_Decl::NT_array)
+	o2be_array::narrow_from_decl(decl)->produce_skel(s,this);
+      else
+	o2be_array::produce_typedef_skel(s,this,
+					 o2be_typedef::narrow_from_decl(base_type()));
+    }
 }
 
 const char*
