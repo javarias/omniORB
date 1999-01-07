97c13766cdbdedd8eaaa7ebdb55cde9f94a16f25
djr <djr@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1999-01-07 18:22:25 +0000

    Replaced _OMNIORB_NTDLL_IMPORT with _LC_attr.

--- old_trunk/omni/include/omniORB2/IIOP.h
+++ old_trunk/omni/include/omniORB2/IIOP.h
@@ -31,6 +31,10 @@
 
 /*
   $Log$
+  Revision 1.7  1998/04/07 19:57:16  sll
+  Replace _OMNIORB2_NTDLL_ specification on class IIOP with
+  _OMNIORB_NTDLL_IMPORT on static member constants.
+
   Revision 1.6  1997/12/09 20:39:38  sll
   Removed profileToEncapStream and EncapStreamToProfile.
 
@@ -45,19 +49,19 @@
 #ifndef __IIOP_H__
 #define __IIOP_H__
 
-class IIOP {
 
-  public:
- 
+class IIOP {
+public:
+
   struct Version { 
     _CORBA_Char major;
     _CORBA_Char minor; 
   };
- 
+
   // Current IIOP Protocol version
-  static _OMNIORB_NTDLL_IMPORT const _CORBA_Char current_major;
-  static _OMNIORB_NTDLL_IMPORT const _CORBA_Char current_minor;
- 
+  static _LC_attr const _CORBA_Char current_major;
+  static _LC_attr const _CORBA_Char current_minor;
+
   struct ProfileBody {
     Version                 iiop_version;
     _CORBA_Char*            host;       // deleted by dtor
--- old_trunk/omni/include/omniORB2/IOP.h
+++ old_trunk/omni/include/omniORB2/IOP.h
@@ -31,6 +31,10 @@
 
 /*
   $Log$
+  Revision 1.4  1998/04/07 19:59:00  sll
+   Replace _OMNIORB2_NTDLL_ specification on class IOP with
+  _OMNIORB_NTDLL_IMPORT on static member constants.
+
   Revision 1.3  1997/12/09 20:36:28  sll
   Added TaggedProfileList_var class.
 
@@ -42,11 +46,12 @@
 #ifndef __IOP_H__
 #define __IOP_H__
 
+
 class IOP {
 public:
   typedef _CORBA_ULong ProfileId;
-  static _OMNIORB_NTDLL_IMPORT const   ProfileId    TAG_INTERNET_IOP;
-  static _OMNIORB_NTDLL_IMPORT const   ProfileId    TAG_MULTIPLE_COMPONENTS;
+  static _LC_attr const   ProfileId    TAG_INTERNET_IOP;
+  static _LC_attr const   ProfileId    TAG_MULTIPLE_COMPONENTS;
 
   struct TaggedProfile {
     ProfileId tag;
@@ -158,7 +163,7 @@ public:
 
   typedef _CORBA_Unbounded_Sequence<ServiceContext> ServiceContextList;
 
-  static _OMNIORB_NTDLL_IMPORT const ServiceID TransactionService;
+  static _LC_attr const ServiceID TransactionService;
 
 
   // omniORB2 private function
@@ -177,5 +182,6 @@ public:
   // (ref CORBA 2 spec. 10.6.5)
 
 };
+
 
 #endif // __IOP_H__
