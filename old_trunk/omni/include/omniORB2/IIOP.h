78ff900e63b3dd6ee3137076db3798f2bf97c24d
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1999-06-18 20:35:53 +0000

    Replaced _LC_attr with _core_attr

--- old_trunk/omni/include/omniORB2/IIOP.h
+++ old_trunk/omni/include/omniORB2/IIOP.h
@@ -31,6 +31,9 @@
 
 /*
   $Log$
+  Revision 1.8  1999/01/07 18:20:05  djr
+  Replaced _OMNIORB_NTDLL_IMPORT with _LC_attr.
+
   Revision 1.7  1998/04/07 19:57:16  sll
   Replace _OMNIORB2_NTDLL_ specification on class IIOP with
   _OMNIORB_NTDLL_IMPORT on static member constants.
@@ -59,8 +62,8 @@ public:
   };
 
   // Current IIOP Protocol version
-  static _LC_attr const _CORBA_Char current_major;
-  static _LC_attr const _CORBA_Char current_minor;
+  static _core_attr const _CORBA_Char current_major;
+  static _core_attr const _CORBA_Char current_minor;
 
   struct ProfileBody {
     Version                 iiop_version;
