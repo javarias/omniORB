ff9e10132a231f853cbac0f30f00212e0554b663
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1999-09-22 18:59:40 +0000

    Added powerpc linux support.

--- old_branches/omni2_8_develop/omni/include/omniORB2/CORBA_sysdep.h
+++ old_branches/omni2_8_develop/omni/include/omniORB2/CORBA_sysdep.h
@@ -32,6 +32,9 @@
 
 /*
  $Log$
+ Revision 1.39.2.1  1999/09/22 13:44:40  sll
+ Updated for AIX 4.2 and 4.3
+
  Revision 1.39  1999/08/30 18:43:34  sll
  Removed ENABLE_CLIENT_IR_SUPPORT as the default define. Application code
  should define this macro if the code is to act as a client to an
@@ -328,6 +331,9 @@
 # if !defined(__WIN32__)
 #  define _HAS_SIGNAL 1
 # endif
+#elif defined(__linux__) && defined(__powerpc__)
+# define _OMNIORB_HOST_BYTE_ORDER_ 0
+# define _HAS_SIGNAL 1
 #elif defined(__aix__) && defined(__powerpc__)
 # define _OMNIORB_HOST_BYTE_ORDER_ 0
 # define _HAS_SIGNAL 1
