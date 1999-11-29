d9d40cd16fa1d970c59b0fd1295e67c5e3fd71f0
dpg1 <dpg1@31ce16a7-c309-44a6-b7c4-d857baf782c7>
1999-11-29 16:43:51 +0000

    Forgot a case in registerDecl().

--- old_trunk/omni/src/tool/omniidl/python/omniidl/idlast.py
+++ old_trunk/omni/src/tool/omniidl/python/omniidl/idlast.py
@@ -28,6 +28,9 @@
 
 # $Id$
 # $Log$
+# Revision 1.12  1999/11/29 15:04:47  dpg1
+# Fixed bug in clear().
+#
 # Revision 1.11  1999/11/25 11:20:33  dpg1
 # Tidy documentation changes.
 #
@@ -911,6 +914,13 @@ def registerDecl(scopedName, decl):
             
             # repeat forward declaration
             rdecl._more.append(decl)
+
+        elif (isinstance(decl, Forward) and isinstance(rdecl, Interface)) or \
+             (isinstance(decl, ValueForward) and \
+              (isinstance(rdecl, ValueAbs) or isinstance(rdecl, Value))):
+
+            # forward declaration of full declaration
+            decl._fullDecl = rdecl
 
         elif isinstance(decl, Module) and \
              isinstance(rdecl, Module):
