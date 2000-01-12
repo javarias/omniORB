f38342caa52289532ce6772afab637a220de56bf
djs <djs@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-01-12 11:52:09 +0000

    Added important comment :)

--- old_trunk/omni/src/lib/omniORB/omniidl_be/cxx/dynskel/__init__.py
+++ old_trunk/omni/src/lib/omniORB/omniidl_be/cxx/dynskel/__init__.py
@@ -28,8 +28,18 @@
 
 # $Id$
 # $Log$
+# Revision 1.2  1999/12/09 20:40:13  djs
+# TypeCode and Any generation option performs identically to old compiler for
+# all current test fragments.
+#
 # Revision 1.1  1999/11/12 17:18:06  djs
 # Skeleton of dynamic skeleton code :)
+#
+
+# All you really need to know for the moment is that the universe
+# is a lot more complicated than you might think, even if you
+# start from a position of thinking it's pretty damn complicated
+# in the first place.       Douglas Adams, "Mostly Harmless"
 #
 
 # -----------------------------
