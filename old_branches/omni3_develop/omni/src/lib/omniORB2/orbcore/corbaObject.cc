2d8a288af097f0d97340b87a92d0591874439c22
dpg1 <dpg1@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-02-07 15:31:44 +0000

    Silly mistake in _is_equivalent() for pseudo object references.

--- old_branches/omni3_develop/omni/src/lib/omniORB2/orbcore/corbaObject.cc
+++ old_branches/omni3_develop/omni/src/lib/omniORB2/orbcore/corbaObject.cc
@@ -28,6 +28,9 @@
  
 /*
   $Log$
+  Revision 1.19.6.7  2000/02/04 18:11:01  djr
+  Minor mods for IRIX (casting pointers to ulong instead of int).
+
   Revision 1.19.6.6  1999/10/29 13:18:16  djr
   Changes to ensure mutexes are constructed when accessed.
 
@@ -144,7 +147,7 @@ CORBA::Object::_is_equivalent(CORBA::Object_ptr other_object)
   // Pseudo objects are equivalent only if pointers are equal.
   // (So the above test should have gotten it).
   if( _NP_is_pseudo() )
-    return other_object->_NP_is_pseudo();
+    return 0;
 
   if( other_object->_NP_is_nil() ) {
     return _NP_is_nil();
