3e216828175f35d607ee693a0829d69bb9edf4f8
dpg1 <dpg1@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-06-07 15:34:52 +0000

    Replace refcounting removed by djr on 31 Jan.

--- old_branches/omni2_8_develop/omni/src/lib/omniORB2/orbcore/strand.cc
+++ old_branches/omni2_8_develop/omni/src/lib/omniORB2/orbcore/strand.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.10.2.3  2000/01/31 11:04:47  djr
+  Improved allocation of strands to client threads.
+
   Revision 1.10.2.2  1999/09/27 13:31:43  djr
   Updated logging to always issue omniORB: prefix.
 
@@ -596,6 +599,10 @@ Strand_iterator::Strand_iterator(const Rope *r,
 
 Strand_iterator::~Strand_iterator()
 {
+  if (pd_s) {
+    pd_s->decrRefCount(1);
+    pd_s = 0;                // Be paranoid
+  }
   if (!pd_leave_mutex)
     ((Rope *)pd_rope)->pd_lock.unlock();
   return;
@@ -605,6 +612,7 @@ Strand *
 Strand_iterator::operator() ()
 {
   if (pd_s) {
+    pd_s->decrRefCount(1);
     pd_s = pd_s->pd_next;
   }
   else if (!pd_initialised) {
@@ -618,6 +626,9 @@ Strand_iterator::operator() ()
       delete p;
     else
       p->~Strand();
+  }
+  if (pd_s) {
+    pd_s->incrRefCount(1);
   }
   return pd_s;
 }
