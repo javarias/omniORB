8d4bc109728b770b15d16d3ee33e571512b5d5a8
dpg1 <dpg1@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-07-24 10:22:03 +0000

    Adding the missing constructor meant that there was no longer a default constructor.

--- old_branches/omni3_develop/omni/src/lib/omniORB2/omniidl_be/cxx/header/template.py
+++ old_branches/omni3_develop/omni/src/lib/omniORB2/omniidl_be/cxx/header/template.py
@@ -28,6 +28,9 @@
 
 # $Id$
 # $Log$
+# Revision 1.3.2.13  2000/07/24 10:17:31  djs
+# Added missing BOA skeleton constructor
+#
 # Revision 1.3.2.12  2000/07/04 12:57:55  djs
 # Fixed Any insertion/extraction operators for unions and exceptions
 #
@@ -344,6 +347,7 @@ class _sk_@name@ :
   @sk_inherits@
 {
 public:
+  _sk_@name@() {}
   _sk_@name@(const omniOrbBoaKey&);
   virtual ~_sk_@name@();
   inline @name@::_ptr_type _this() {
