ec8a5d93633d4381f771aa402e4082823654f4ca
dpg1 <dpg1@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-08-07 17:31:44 +0000

    Minor long long mistake.

--- old_branches/unlabeled-1.9.2/omni/src/lib/omniORB/orbcore/logIOstream.cc
+++ old_branches/unlabeled-1.9.2/omni/src/lib/omniORB/orbcore/logIOstream.cc
@@ -28,6 +28,9 @@
  
 /*
   $Log$
+  Revision 1.9.2.1  2000/08/04 17:10:30  dpg1
+  Long long support
+
   Revision 1.9  2000/07/13 15:25:57  dpg1
   Merge from omni3_develop for 3.0 release.
 
@@ -293,7 +296,7 @@ omniORB::logger::operator<<(unsigned long n)
   return *this;
 }
 
-#ifdef HAS_LongDouble
+#ifdef HAS_LongLong
 omniORB::logger&
 omniORB::logger::operator<<(_CORBA_LONGLONG_DECL n)
 {
