96e2e6ec209f90c613ffc5387766bd48d1d4a6db
nobody <nobody@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-08-07 17:09:18 +0000

    This commit was manufactured by cvs2svn to create tag 'omni3_0_to_3_1_latest'.

---
+++ old_tags/omni3_0_to_3_1_latest/omni/src/services/mklib/mkBOAlib/dir.mk
@@ -0,0 +1,162 @@
+include ../cosinterfaces.mk
+include ../libdefs.mk
+
+# Generate BOA skeleton
+DIR_IDLFLAGS += -WbBOA
+
+# Look for .idl files in <top>/idl plus ../../idl
+vpath %.idl $(IMPORT_TREES:%=%/idl) $(VPATH:%=%/../../idl)
+
+DIR_IDLFLAGS += -I. $(patsubst %,-I%/../../idl,$(VPATH)) \
+                   $(patsubst %,-I%/idl,$(IMPORT_TREES))
+
+
+COS_SKLIB_NAME    = COS_BOA
+
+COS_SK_OBJS = $(COS_INTERFACES:%=%SK.o)
+COS_SK_SRCS = $(COS_INTERFACES:%=%SK.cc)
+CXXSRCS = $(COS_SK_SRCS) 
+
+all:: mkstatic mkshared
+
+export:: mkstatic mkshared
+
+export:: $(COS_INTERFACES:%=%.hh)
+	@(for i in $^; do \
+            file="$$i"; \
+            dir="$(EXPORT_TREE)/$(INCDIR)/COS/BOA"; \
+		$(ExportFileToDir) \
+          done )
+
+veryclean::
+	$(RM) $(COS_INTERFACES:%=%SK.cc) $(COS_INTERFACES:%=%DynSK.cc) \
+              $(COS_INTERFACES:%=%.hh)
+
+##############################################################################
+# Build Static library
+##############################################################################
+
+version  := $(word 1,$(subst ., ,$(OMNIORB_VERSION)))
+
+sk = static/$(patsubst %,$(LibNoDebugPattern),$(COS_SKLIB_NAME)$(version))
+
+mkstatic::
+	@(dir=static; $(CreateDir))
+
+mkstatic:: $(sk)
+
+$(sk): $(patsubst %, static/%, $(COS_SK_OBJS))
+	@$(StaticLinkLibrary)
+
+export:: $(sk)
+	@$(ExportLibrary)
+
+clean::
+	$(RM) static/*.o
+	$(RM) $(sk)
+
+veryclean::
+	$(RM) static/*.o
+	$(RM) $(sk)
+
+
+##############################################################################
+# Build Shared library
+##############################################################################
+ifdef BuildSharedLibrary
+
+sharedversion = $(OMNIORB_VERSION)
+
+sknamespec    = $(subst ., ,$(COS_SKLIB_NAME).$(sharedversion))
+skshared      = shared/$(shell $(SharedLibraryFullName) $(sknamespec))
+
+mkshared::
+	@(dir=shared; $(CreateDir))
+
+mkshared:: $(skshared)
+
+$(skshared): $(patsubst %, shared/%, $(COS_SK_OBJS))
+	@(namespec="$(sknamespec)" extralibs="$(OMNIORB_LIB)"; \
+         $(MakeCXXSharedLibrary))
+
+export:: $(skshared)
+	@(namespec="$(sknamespec)"; \
+         $(ExportSharedLibrary))
+
+clean::
+	$(RM) shared/*.o
+	(dir=shared; $(CleanSharedLibrary))
+
+veryclean::
+	$(RM) shared/*.o
+	@(dir=shared; $(CleanSharedLibrary))
+
+endif
+
+##############################################################################
+# Build debug libraries for Win32
+##############################################################################
+ifdef Win32Platform
+
+all:: mkstaticdbug mkshareddbug
+
+export:: mkstaticdbug mkshareddbug
+
+#####################################################
+#      Static debug libraries
+#####################################################
+dbugversion = $(word 1,$(subst ., ,$(OMNIORB_VERSION)))
+
+skdbug = debug/$(patsubst %,$(LibDebugPattern),$(COS_SKLIB_NAME)$(dbugversion))
+
+mkstaticdbug::
+	@(dir=debug; $(CreateDir))
+
+mkstaticdbug:: $(skdbug)
+
+$(skdbug): $(patsubst %, debug/%, $(COS_SK_OBJS))
+	@$(StaticLinkLibrary)
+
+export:: $(skdbug)
+	@$(ExportLibrary)
+
+clean::
+	$(RM) debug/*.o
+	$(RM) $(skdbug)
+
+veryclean::
+	$(RM) debug/*.o
+	$(RM) $(skdbug)
+
+#####################################################
+#      DLL debug libraries
+#####################################################
+shareddbugversion = $(OMNIORB_VERSION)
+
+sknamespec      = $(subst ., ,$(COS_SKLIB_NAME).$(shareddbugversion))
+skshareddbug    = shareddebug/$(shell $(SharedLibraryDebugFullName) $(sknamespec))
+
+mkshareddbug::
+	@(dir=shareddebug; $(CreateDir))
+
+mkshareddbug:: $(skshareddbug)
+
+$(skshareddbug): $(patsubst %, shareddebug/%, $(COS_SK_OBJS))
+	(namespec="$(sknamespec)" debug=1 extralibs="$(OMNIORB_LIB)"; \
+         $(MakeCXXSharedLibrary))
+
+export:: $(skshareddbug)
+	@(namespec="$(sknamespec)" debug=1; \
+         $(ExportSharedLibrary))
+
+clean::
+	$(RM) shareddebug/*.o
+	@(dir=shareddebug; $(CleanSharedLibrary))
+
+veryclean::
+	$(RM) shareddebug/*.o
+	@(dir=shareddebug; $(CleanSharedLibrary))
+
+endif
+
+
