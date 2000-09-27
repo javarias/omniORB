cb5e1578ed37de797ab3239b08b502b8eee8f1c5
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-09-27 17:12:03 +0000

    Updated to include from include/omniORB4.

--- old_branches/omni4_0_develop/omni/include/omniORB4/omniURI.h
+++ old_branches/omni4_0_develop/omni/include/omniORB4/omniURI.h
@@ -32,6 +32,9 @@
 
 // $Id$
 // $Log$
+// Revision 1.2.2.1  2000/07/17 10:35:35  sll
+// Merged from omni3_develop the diff between omni3_0_0_pre3 and omni3_0_0.
+//
 // Revision 1.3  2000/07/13 15:26:05  dpg1
 // Merge from omni3_develop for 3.0 release.
 //
@@ -45,8 +48,8 @@
 #define _omniURI_h_
 
 
-#include <omniORB3/CORBA.h>
-#include <omniORB3/Naming.hh>
+#include <omniORB4/CORBA.h>
+#include <omniORB4/Naming.hh>
 
 
 class omniURI {
