ccbf9735232dc14489b65d6feea6c188ef12fb84
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-09-27 17:12:47 +0000

    New member _upcall().

--- old_branches/omni4_0_develop/omni/include/omniORB4/omniServant.h
+++ old_branches/omni4_0_develop/omni/include/omniORB4/omniServant.h
@@ -29,6 +29,9 @@
  
 /*
   $Log$
+  Revision 1.2.2.1  2000/07/17 10:35:35  sll
+  Merged from omni3_develop the diff between omni3_0_0_pre3 and omni3_0_0.
+
   Revision 1.3  2000/07/13 15:26:05  dpg1
   Merge from omni3_develop for 3.0 release.
 
@@ -134,6 +137,10 @@ public:
   }
   // Must hold <omni::internalLock>.
 
+protected:
+  void _upcall(GIOP_S&, omniCallDescriptor&);
+  // Called by the dispatcher in the skeleton implementation stub to drive the 
+  // upcall into the implementation code.
 
 private:
   omniLocalIdentity* pd_identities;
