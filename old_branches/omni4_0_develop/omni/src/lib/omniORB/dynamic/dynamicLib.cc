dc2671173fe8fa32475d570d97c97ba0a255fc53
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-10-09 16:27:02 +0000

    Temporary disable IR support.

--- old_branches/omni4_0_develop/omni/src/lib/omniORB/dynamic/dynamicLib.cc
+++ old_branches/omni4_0_develop/omni/src/lib/omniORB/dynamic/dynamicLib.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.2.2.3  2000/10/06 16:40:54  sll
+  Changed to use cdrStream.
+
   Revision 1.2.2.2  2000/09/27 17:25:42  sll
   Changed include/omniORB3 to include/omniORB4.
 
@@ -43,7 +46,8 @@
 
 */
 
-#define ENABLE_CLIENT_IR_SUPPORT
+// XXX Temporary disable ENABLE_CLIENT_IR_SUPPORT
+//#define ENABLE_CLIENT_IR_SUPPORT
 #include <omniORB4/CORBA.h>
 #include <omniORB4/callDescriptor.h>
 #include <dynamicLib.h>
@@ -100,7 +104,13 @@ marshal_context(cdrStream& s, CORBA::Context_ptr ctxt,
 static void
 lookup_id_lcfn(omniCallDescriptor* cd, omniServant* svnt)
 {
+  // XXX
+  throw omniORB::fatalException(__FILE__,__LINE__,
+				"Not ported yet.");
+
+#if 0
   omniStdCallDesc::_cCORBA_mObject_i_cstring* tcd = (omniStdCallDesc::_cCORBA_mObject_i_cstring*) cd;
   CORBA::_impl_Repository* impl = (CORBA::_impl_Repository*) svnt->_ptrToInterface(CORBA::Repository::_PD_repoId);
   tcd->pd_result = impl->lookup_id(tcd->arg_0);
+#endif
 }
