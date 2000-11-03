73f9331599b6b87eb4c7fe57b05bd464f181957b
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-11-03 19:08:52 +0000

    Use virtual dtor with gcc.

--- old_branches/omni4_0_develop/omni/src/lib/omniORB/omniIdentity.h
+++ old_branches/omni4_0_develop/omni/src/lib/omniORB/omniIdentity.h
@@ -30,6 +30,9 @@
 
 /*
   $Log$
+  Revision 1.2.2.2  2000/09/27 17:39:28  sll
+  Updated include/omniORB3 to include/omniORB4
+
   Revision 1.2.2.1  2000/07/17 10:35:40  sll
   Merged from omni3_develop the diff between omni3_0_0_pre3 and omni3_0_0.
 
@@ -71,10 +74,16 @@ public:
 
 
 protected:
+#ifndef __GNUG__ 
   inline ~omniIdentity() {}
   // Should only be destroyed by implementation of derived classes.
   // This doesn't need to be virtual, since it is only ever deleted
   // by the most derived type.
+#else
+  virtual ~omniIdentity() {}
+  // But gcc is rather anal and insits on a class with virtual functions must
+  // have a virtual dtor.
+#endif
 
   inline omniIdentity(omniObjKey& key)
     : pd_key(key, 1) {}
