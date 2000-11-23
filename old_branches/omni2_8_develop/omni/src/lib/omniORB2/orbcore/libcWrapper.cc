885f2cbd303c1180aec089ae29d87df6e84ac533
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2000-11-23 11:33:43 +0000

    DU/Tru64 requires the buffer to be cleared before passing it to gethostbyname_r().

--- old_branches/omni2_8_develop/omni/src/lib/omniORB2/orbcore/libcWrapper.cc
+++ old_branches/omni2_8_develop/omni/src/lib/omniORB2/orbcore/libcWrapper.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.18  1999/06/26 18:08:17  sll
+  HPUX update to separate difference between HPUX 10.20 and HPUX 11.00.
+
   Revision 1.17  1999/03/11 16:25:54  djr
   Updated copyright notice
 
@@ -115,6 +118,7 @@ again:
   // Use gethostbyname_r() on Digital UNIX
   if (!h.pd_buffer) {
     h.pd_buffer = new char[sizeof(struct hostent_data)];
+    memset(h.pd_buffer,0,sizeof(struct hostent_data));
     // XXX Is it possible that the pointer h.pd_buffer is at a wrong alignment
     //     for a struct hostent_data?
     h.pd_buflen = sizeof(struct hostent_data);
