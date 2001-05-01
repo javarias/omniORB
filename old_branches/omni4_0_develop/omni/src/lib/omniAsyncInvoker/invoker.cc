3bc1df9716fb3290ecfcaa581511c74dfe834318
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2001-05-01 16:03:16 +0000

    Silly typo in a switch statement causes random failure due to corrupted link list.

--- old_branches/omni4_0_develop/omni/src/lib/omniAsyncInvoker/invoker.cc
+++ old_branches/omni4_0_develop/omni/src/lib/omniAsyncInvoker/invoker.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.1.2.1  2001/04/19 09:47:54  sll
+  New library omniAsyncInvoker.
+
 */
 
 #include <omnithread.h>
@@ -235,8 +238,8 @@ omniAsyncInvoker::insert(omniTask* t) {
 	  // Cannot start a new thread.
 	  return 0;
 	}
-	break;
       }
+      break;
     }
   case omniTask::DedicatedThread:
     {
