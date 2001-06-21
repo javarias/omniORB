8b204f22722044aa6808fffcbf662c13fe42bfa5
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2001-06-21 11:17:17 +0000

    Added darwin port.

--- old_branches/omni4_0_develop/omni/src/tool/omniidl/cxx/idlutil.cc
+++ old_branches/omni4_0_develop/omni/src/tool/omniidl/cxx/idlutil.cc
@@ -28,6 +28,9 @@
 
 // $Id$
 // $Log$
+// Revision 1.5.2.4  2001/06/08 17:12:24  dpg1
+// Merge all the bug fixes from omni3_develop.
+//
 // Revision 1.5.2.3  2000/10/27 16:31:10  dpg1
 // Clean up of omniidl dependencies and types, from omni3_develop.
 //
@@ -154,12 +157,34 @@ idl_strtoul(const char* text, int base)
   return strtoul(text, 0, base);
 }
 
-#  elif defined(__freebsd__)
+#  elif defined(__freebsd__) || defined (__darwin__)
 
 IdlIntLiteral
 idl_strtoul(const char* text, int base)
 {
   return strtouq(text, 0, base);
+}
+
+#  elif defined(__hpux__)
+
+IdlIntLiteral
+idl_strtoul(const char* text, int base)
+{
+  IdlIntLiteral ull;
+  switch (base) {
+  case 8:
+    sscanf(text, "%llo", &ull);
+    break;
+  case 10:
+    sscanf(text, "%lld", &ull);
+    break;
+  case 16:
+    sscanf(text, "%llx", &ull);
+    break;
+  default:
+    abort();
+  }
+  return ull;
 }
 
 #  else
