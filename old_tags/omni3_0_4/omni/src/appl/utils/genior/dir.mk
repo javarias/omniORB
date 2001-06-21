fa5eb5326efb732ee926ee510af2424b7c72cb85
nobody <nobody@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2001-06-21 14:02:20 +0000

    This commit was manufactured by cvs2svn to create tag 'omni3_0_4'.

---
+++ old_tags/omni3_0_4/omni/src/appl/utils/genior/dir.mk
@@ -0,0 +1,17 @@
+CXXSRCS = genior.cc
+
+CorbaImplementation = OMNIORB
+DIR_CPPFLAGS = $(CORBA_CPPFLAGS)
+
+GENIOR = $(patsubst %,$(BinPattern),genior)
+
+all:: $(GENIOR)
+
+clean::
+	$(RM) $(GENIOR)
+
+$(GENIOR): genior.o $(CORBA_LIB_NODYN_DEPEND)
+	@(libs="$(CORBA_LIB_NODYN)"; $(CXXExecutable))
+
+export:: $(GENIOR)
+	@$(ExportExecutable)
