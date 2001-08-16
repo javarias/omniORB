5e5588d99931a21b735b8066268e6a302b09ad0b
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2001-08-16 16:12:19 +0000

    Removed duplicate offerBidirectionalGIOP.

--- old_branches/omni4_0_develop/omni/include/omniORB4/omniORB.h
+++ old_branches/omni4_0_develop/omni/include/omniORB4/omniORB.h
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.2.2.12  2001/08/08 15:57:11  sll
+  New options unixTransportDirectory & unixTransportPermission.
+
   Revision 1.2.2.11  2001/08/03 17:45:08  sll
   Moved OMNIORB_THROW so that it can be used in stub headers
 
@@ -732,17 +735,6 @@ _CORBA_MODULE_BEG
   _CORBA_MODULE_VAR _core_attr CORBA::UShort unixTransportPermission;
   // Default is 0777.               		                        //
 
-
-  ////////////////////////////////////////////////////////////////////////
-  // offerBiDirectionalGIOP   	                                        //
-  //   Applies to the server side. Set to TRUE to indicates that the    //
-  //   ORB may choose to accept a clients offer to use bidirectional    //
-  //   GIOP calls on a connection. Set to FALSE means the ORB should    //
-  //   never accept any bidirectional offer and should stick to normal  //
-  //   GIOP.   	       	       	       	       	       	       	        //
-  //   	       	       	       	       	       	       	       	        //
-  _CORBA_MODULE_VAR _core_attr CORBA::Boolean offerBiDirectionalGIOP;
-  // false by default.				                        //
 
   ////////////////////////////////////////////////////////////////////////
   // diiThrowsSysExceptions                                             //
