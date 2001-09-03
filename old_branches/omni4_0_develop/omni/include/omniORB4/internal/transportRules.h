c7b41df0315bf1c8a7f8b5f39223a69a0b482135
sll <sll@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2001-09-03 13:27:12 +0000

    Minor update to comments.

--- old_branches/omni4_0_develop/omni/include/omniORB4/internal/transportRules.h
+++ old_branches/omni4_0_develop/omni/include/omniORB4/internal/transportRules.h
@@ -28,6 +28,9 @@
 
 /*
   $Log$
+  Revision 1.1.2.2  2001/08/29 17:50:39  sll
+  New method dumpRule.
+
   Revision 1.1.2.1  2001/08/23 16:00:35  sll
   Added method in giopTransportImpl to return the addresses of the host
   interfaces.
@@ -94,7 +97,7 @@ class transportRules {
 		       CORBA::ULong& priority/* return arg */);
   // Return true if <endpoint> matches one of the transport rules.
   // The action list of the matched rule is returned in <actions>.
-  // The priority of the matched rule is returned in <priority>.
+  // The index of the matched rule is returned in <priority>.
   // Return false if <endpoint> does not match any rule. In that case
   // <actions and <priority> are not initialised.
 
