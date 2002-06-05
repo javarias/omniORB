ab97f7c8cc0c8ad2a302f5f00c88e2f80482f35d
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2002-06-05 22:26:32 +0000

    Cope with spaces in paths to IDL files.

--- old_branches/omni4_0_develop/omni/src/tool/omniidl/python/omniidl/main.py
+++ old_branches/omni4_0_develop/omni/src/tool/omniidl/python/omniidl/main.py
@@ -29,6 +29,9 @@
 
 # $Id$
 # $Log$
+# Revision 1.17.2.9  2002/04/25 23:12:44  dgrisby
+# Bug with invalid omniidl option reporting.
+#
 # Revision 1.17.2.8  2002/04/25 20:33:20  dgrisby
 # Better job of looking for omnicpp.
 #
@@ -383,12 +386,13 @@ def main(argv=None):
             sys.exit(1)
 
  	if sys.platform != 'OpenVMS' or len(preprocessor_args)==0:
- 	    preproc_cmd = preprocessor_cmd + " " + \
- 			  string.join(preprocessor_args, " ") + " " + file
+ 	    preproc_cmd = '%s %s "%s"' % (preprocessor_cmd,
+                                          string.join(preprocessor_args, ' '),
+                                          file)
  	else:
- 	    preproc_cmd = preprocessor_cmd + ' "' + \
-                       string.join(preprocessor_args, '" "') + '" ' + file
-
+            preproc_cmd = '%s "%s" %s' % (preprocessor_cmd,
+                                          string.join(preprocessor_args,'" "'),
+                                          file)
         if not no_preprocessor:
             if verbose:
                 sys.stderr.write(cmdname + ": Preprocessing '" +\
