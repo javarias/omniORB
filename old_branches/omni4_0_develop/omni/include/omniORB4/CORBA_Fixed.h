0bac5a03b5b9ea1f51e57d37c83a005a6df931a9
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2002-10-15 23:24:39 +0000

    Avoid clash with Solaris defining truncate to truncate64.

--- old_branches/omni4_0_develop/omni/include/omniORB4/CORBA_Fixed.h
+++ old_branches/omni4_0_develop/omni/include/omniORB4/CORBA_Fixed.h
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.1.2.2  2001/10/17 16:44:00  dpg1
+  Update DynAny to CORBA 2.5 spec, const Any exception extraction.
+
   Revision 1.1.2.1  2001/08/17 13:39:46  dpg1
   Split CORBA.h into separate bits.
 
@@ -37,6 +40,12 @@
 #ifndef INSIDE_OMNIORB_CORBA_MODULE
 #  error "Must only be #included by CORBA.h"
 #endif
+
+// Some Solaris versions helpfully #define truncate to truncate64...
+#ifdef truncate
+#  undef truncate
+#endif
+
 
 //////////////////////////////////////////////////////////////////////
 /////////////////////////////// Fixed ////////////////////////////////
