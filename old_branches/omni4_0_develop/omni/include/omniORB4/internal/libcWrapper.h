cb90d441cc3b4d6dd58d11723e17066396d5e59c
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2004-03-19 01:08:35 +0000

    Fix Windows DLL issues.

--- old_branches/omni4_0_develop/omni/include/omniORB4/internal/libcWrapper.h
+++ old_branches/omni4_0_develop/omni/include/omniORB4/internal/libcWrapper.h
@@ -30,6 +30,9 @@
 
 /*
   $Log$
+  Revision 1.1.4.10  2003/11/06 10:18:39  dgrisby
+  Expand FD_SETSIZE on Windows.
+
   Revision 1.1.4.9  2003/07/25 16:00:51  dgrisby
   Avoid linker warnings on Windows.
 
@@ -154,17 +157,15 @@ public:
 
 OMNI_NAMESPACE_END(omni)
 
-#if defined(_MSC_VER)
-#  if defined(_OMNIORB_DYNAMIC_LIBRARY)
+#if defined(_MSC_VER) && !defined(_WINSTATIC)
+#  if defined(_OMNIORB_LIBRARY)
 #    define _NT_DLL_ATTR __declspec(dllexport)
-#  elif !defined(_WINSTATIC)
-#    define _NT_DLL_ATTR __declspec(dllimport)
-#  elif defined(_OMNIORB_LIBRARY)
-#    define _NT_DLL_ATTR
 #  else
-#    define _NT_DLL_ATTR
+#    define _NT_DLL_ATTR __declspec(dllimport)
 #  endif
-#else
+#endif
+
+#ifndef _NT_DLL_ATTR
 #  define _NT_DLL_ATTR
 #endif
 
