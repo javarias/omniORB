c2b425296f1961114de1276354f0b1cf390c441f
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2004-10-17 22:29:06 +0000

    WChar TypeCode unmarshalling was missing. Thanks Luke Deller.

--- old_branches/omni4_0_develop/omni/src/lib/omniORB/dynamic/typecode.cc
+++ old_branches/omni4_0_develop/omni/src/lib/omniORB/dynamic/typecode.cc
@@ -29,6 +29,9 @@
 
 /*
  * $Log$
+ * Revision 1.38.2.30  2003/03/10 11:13:52  dgrisby
+ * BAD_PARAM with invalid fixed limits.
+ *
  * Revision 1.38.2.29  2003/03/05 15:26:54  dgrisby
  * Missing Fixed typecode unmarshal. Thanks Renzo Tomaselli.
  *
@@ -4449,6 +4452,9 @@ TypeCode_marshaller::unmarshal(cdrStream& s,
     case CORBA::tk_char:
       otbl->addEntry(otbl->currentOffset(), ToTcBase(CORBA::_tc_char));
       return TypeCode_collector::duplicateRef(ToTcBase(CORBA::_tc_char));
+    case CORBA::tk_wchar:
+      otbl->addEntry(otbl->currentOffset(), ToTcBase(CORBA::_tc_wchar));
+      return TypeCode_collector::duplicateRef(ToTcBase(CORBA::_tc_wchar));
     case CORBA::tk_octet:
       otbl->addEntry(otbl->currentOffset(), ToTcBase(CORBA::_tc_octet));
       return TypeCode_collector::duplicateRef(ToTcBase(CORBA::_tc_octet));
