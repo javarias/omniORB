6437bb58df4fe9c219dff15fe84bcc99d5de1637
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2005-06-08 09:37:47 +0000

    Leak of a value reference if a factory made the wrong type of value.

--- old_branches/omni4_1_develop/omni/src/lib/omniORB/dynamic/valueType.cc
+++ old_branches/omni4_1_develop/omni/src/lib/omniORB/dynamic/valueType.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.1.2.6  2004/10/13 17:58:21  dgrisby
+  Abstract interfaces support; values support interfaces; value bug fixes.
+
   Revision 1.1.2.5  2004/07/26 22:56:39  dgrisby
   Support valuetypes in Anys.
 
@@ -598,8 +601,10 @@ handleIncompatibleValue(const char* repoId, CORBA::ULong hashval,
     }
 
     // Factory is now available. Did it make a suitable value?
-    if (!result->_ptrToValue(repoId))
+    if (!result->_ptrToValue(repoId)) {
+      CORBA::remove_ref(result);
       OMNIORB_THROW(BAD_PARAM, BAD_PARAM_ValueFactoryFailure, completion);
+    }
 
     if (omniORB::trace(25)) {
       omniORB::logger l;
