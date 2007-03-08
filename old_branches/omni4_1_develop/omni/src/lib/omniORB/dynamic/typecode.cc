2c2ed8a283eb7ab0d153b9288ea1639cf9a64800
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2007-03-08 09:17:11 +0000

    Union discriminator bug is really fixed now.

--- old_branches/omni4_1_develop/omni/src/lib/omniORB/dynamic/typecode.cc
+++ old_branches/omni4_1_develop/omni/src/lib/omniORB/dynamic/typecode.cc
@@ -31,6 +31,10 @@
 
 /*
  * $Log$
+ * Revision 1.40.2.14  2007/03/07 18:30:24  dgrisby
+ * Bug creating union TypeCode with an enum discriminator. Thanks Peter
+ * S. Housel.
+ *
  * Revision 1.40.2.13  2006/12/28 18:10:24  dgrisby
  * When releasing children of a struct, union or value TypeCode, a
  * reference to the parent must be held to prevent its premature
@@ -6041,7 +6045,7 @@ TypeCode_union_helper::extractLabel(const CORBA::Any& label,
 			BAD_PARAM_IncompatibleDiscriminatorType,
 			CORBA::COMPLETED_NO);
 	CORBA::ULong c;
-        cdrAnyMemoryStream& ms = label.PR_streamToRead();
+        cdrAnyMemoryStream ms(label.PR_streamToRead(), 1);
         c <<= ms;
 	lbl_value = c;
 	break;
