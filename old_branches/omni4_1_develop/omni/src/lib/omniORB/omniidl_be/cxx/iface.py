18efb88d68b8776070af644490d05d1b46313b2e
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2008-12-30 15:38:27 +0000

    Scope change broke structs defined in interfaces that use them.

--- old_branches/omni4_1_develop/omni/src/lib/omniORB/omniidl_be/cxx/iface.py
+++ old_branches/omni4_1_develop/omni/src/lib/omniORB/omniidl_be/cxx/iface.py
@@ -28,6 +28,9 @@
 
 # $Id$
 # $Log$
+# Revision 1.1.6.16  2008/12/29 18:03:34  dgrisby
+# More C++ backend scope errors. Thanks Gellule Xg.
+#
 # Revision 1.1.6.15  2007/12/05 11:15:58  dgrisby
 # Incorrect generated code involving ::CORBA::AbstractBase.
 #
@@ -254,7 +257,7 @@ class _objref_Method(cxx.Method):
   def _from_Callable(self, use_out):
     # Grab the IDL environment
     ifc = self.callable().interface()
-    environment = ifc.environment().enter(ifc.name().simple())
+    environment = ifc.environment().enter("_objref_" + ifc.name().simple())
 
     # Kept as a type object because in .cc part the _return_ type
     # must be fully qualified.
@@ -685,7 +688,8 @@ class _objref_I(Class):
                  name = method.name())
 
 
-      intf_env = self._environment.enter(self.interface().name().simple())
+      intf_env = self._environment.enter("_objref_" +
+                                         self.interface().name().simple())
 
       call_descriptor.out_objrefcall(body,
                                      callable.operation_name(),
