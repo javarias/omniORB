25b81e4e0fe4bbf0dfc4a0175189457a6d7b7e72
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2013-02-08 22:33:46 +0000

    Remove local omnithread.

--- cost/att/testsuite/common/dir.mk
+++ cost/att/testsuite/common/dir.mk
@@ -13,18 +13,6 @@ ifndef Use_omniORB
 CXXSRCS += omnitest_extras.cc
 TOBJS   += omnitest_extras.o
 
-ifeq ($(ThreadSystem),Posix)
-CXXSRCS += omnithread/posix.cc
-TOBJS   += posix.o
-DIR_CPPFLAGS += $(OMNITHREAD_CPPFLAGS) $(OMNITHREAD_POSIX_CPPFLAGS)
-endif
-
-ifeq ($(ThreadSystem),NT)
-CXXSRCS += omnithread/nt.cc
-TOBJS   += nt.o
-DIR_CPPFLAGS += $(OMNITHREAD_CPPFLAGS)
-endif
-
 endif
 
 
@@ -34,12 +22,5 @@ all:: $(tlib)
 
 $(tlib): $(TOBJS)
 	@$(StaticLinkLibrary)
-
-
-posix.o: omnithread/posix.cc
-	$(CXX) -c $(CXXFLAGS) -o $@ $<
-
-nt.o: omnithread/nt.cc
-	$(CXX) -c $(CXXFLAGS) -o $@ $<
 
 include ../mk/afterdir.mk
