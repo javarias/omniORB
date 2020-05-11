55d9775e54ea6a517b5f86aec1850bf581d31771
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2020-05-11 22:49:13 +0000

    Merge fixes from 4_2.

--- branches/4_3/omniORB/src/lib/omniORB/orbcore/ssl/sslConnection.cc
+++ branches/4_3/omniORB/src/lib/omniORB/orbcore/ssl/sslConnection.cc
@@ -131,6 +131,7 @@ sslConnection::Send(void* buf, size_t sz,
 	}
 	else if (tx == RC_SOCKET_ERROR) {
 	  if (ERRNO == RC_EINTR) {
+	    SET_ERRNO(0);
 	    continue;
           }
 	  else {
@@ -226,6 +227,7 @@ sslConnection::Recv(void* buf, size_t sz,
       }
       else if (rx == RC_SOCKET_ERROR) {
 	if (ERRNO == RC_EINTR) {
+	  SET_ERRNO(0);
 	  continue;
         }
 	else {
